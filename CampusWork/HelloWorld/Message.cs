273168d5e450be20a6cc13a44497813e844eec05
Eric LE DEVEDEC <ledevedec.eric@gmail.com>
2018-06-19 15:22:12 +0200

    Fini

---
+++ CampusWork/HelloWorld/Message.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Message
+    {
+
+        public DateTime pDate { get; set; }
+
+        public int pHours { get; set; }
+
+        public DayOfWeek pDay { get; set; }
+
+        public string pUserName { get; set; }
+
+        public string pMessage { get; set; }
+
+        public int pLimitHoursMorning { get; set; }
+
+        public int pLimitHoursAfternoon { get; set; }
+
+        public int pLimitHoursEvening { get; set; }
+
+
+
+        public Message(DateTime aDate, string aUserName, int aLimitHoursMorning = 9, int aLimitHoursAfternoon = 13, int aLimitHoursEvening = 18)
+        {
+
+            pDate = aDate;
+            pHours = aDate.Hour;
+            pDay = aDate.DayOfWeek;
+            pUserName = aUserName;
+            pLimitHoursMorning = aLimitHoursMorning;
+            pLimitHoursAfternoon = aLimitHoursAfternoon;
+            pLimitHoursEvening = aLimitHoursEvening;
+        }
+
+        public Message()
+        {
+
+        }
+
+
+        public string getHelloMessage()
+        {
+
+            if ((pDay.Equals(DayOfWeek.Saturday) || pDay.Equals(DayOfWeek.Sunday))
+                         || (pHours < pLimitHoursMorning && pDay == DayOfWeek.Monday)
+                         || (pHours >= pLimitHoursEvening && pDay.Equals(DayOfWeek.Friday)))
+            {
+
+                pMessage = $"Bon week-end {pUserName}";
+
+            }
+            else if (pHours >= pLimitHoursMorning && pHours < pLimitHoursAfternoon)
+            {
+                pMessage = $"Bonjour {pUserName}";
+
+            }
+
+            else if (pHours >= pLimitHoursAfternoon && pHours < pLimitHoursEvening)
+            {
+                pMessage = $"Bon après-midi {pUserName}";
+
+            }
+
+            else
+            {
+                pMessage = $"Bonsoir {pUserName}";
+
+            }
+            return pMessage;
+
+        }
+
+    }
+}
+
--- CampusWork/HelloWorld/Program.cs
+++ CampusWork/HelloWorld/Program.cs
@@ -6,17 +6,41 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+
+
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            Program wProg = new Program();
+            wProg.startProg();
+
+        }
+
+
+
+        public void startProg()
+        {
+
             string wUser = Environment.UserName;
-            DateTime wDayTest = new DateTime(2018, 6, 19, 14, 00, 00);
+
+            DateTime wDayTest = new DateTime(2018, 6, 25, 8, 00, 00);
 
             Message wMessage = new Message(wDayTest, wUser);
+           
 
-            Console.Write(wMessage.getHelloMessage());
-            Console.ReadKey();
+            String wAnswer;
+
+            do
+            {
+                Console.WriteLine(wMessage.getHelloMessage());
+                wAnswer = Console.ReadLine();
+
+            }
+            while (wAnswer != "exit");
+
         }
     }
 }
