409be26a9f66ade3f157c651bf0bf2737d75157c
Eric LE DEVEDEC <ledevedec.eric@gmail.com>
2018-06-25 16:54:13 +0200

    ok

--- Iteration_2/MetroApp/Map.xaml.cs
+++ Iteration_2/MetroApp/Map.xaml.cs
@@ -1,37 +1,27 @@
 ﻿using MetroLibrary;
 using Microsoft.Maps.MapControl.WPF;
-using Microsoft.Maps.MapControl.WPF.Design;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MetroApp
 {
-   
+
     public partial class Map : Page
     {
 
-        LocationConverter locConverter = new LocationConverter();
+       
 
         public List<StopLines> pStopLinesForMAP { get; set; }
         public string pLongitudePlace { get; set; }
         public string pLatitudePlace { get; set; }
         public string pRayonPlace { get; set; }
 
-        public Map()
-        {
-        }
+        public Map(){}
 
         public Map(List<StopLines> aStopLines, string aLongitudePlace, string aLatitudePlace, string aRayonPlace)
 
@@ -49,7 +39,6 @@ namespace MetroApp
             this.showPinInMap();
 
         }
-
 
         public void MapWithPushpins_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
@@ -104,7 +93,6 @@ namespace MetroApp
             setPinPush(pStopLinesForMAP);
        
         }
-
 
         private void setPinPush(List<StopLines> aStopLinesForMAP)
         {
